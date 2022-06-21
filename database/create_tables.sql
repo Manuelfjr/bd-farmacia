a709c1e97dedc6a8e86acb67c276c7f458a51a85
Wanusa Araújo de Pontes <wanusa.pontes@ltm.digital>
2022-06-21 09:15:24 -0300

    Including Sql

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -19,3 +19,44 @@ CREATE TABLE Funcionarios(
 	sexo varchar(255),
 	FOREIGN KEY (id_filial) REFERENCES Filial(id_filial)
 );
+
+
+CREATE TABLE Remedios(
+    id_remedio INT NOT NULL,
+    Nome_Fantasia varchar(50) NOT NULL,
+    Principio_ativo varchar(50) NOT NULL,
+    Remedio_generico varchar(5) NOT NULL,
+    Valor_Custo float NOT NULL,
+    Valor_Venda float NOT NULL,
+    Tarja_Preta varchar(5) NOT NULL,
+    Fornecedor varchar(50) NOT NULL
+);
+
+
+CREATE TABLE Estoque(
+    id_estoque INT NOT NULL,
+    id_filial INT NOT NULL,
+    id_remedio INT NOT NULL,
+    qtd_remedio INT NOT NULL
+);
+
+
+CREATE TABLE Promocao(
+    id_promocao int not null,
+    id_remedio int not null,
+    valor_promocional float not null
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
